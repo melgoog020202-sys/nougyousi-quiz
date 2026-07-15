00f333d47054d441a7976bdfafa91cd6cdaded43
melgoog020202-sys <melgoog020202@gmail.com>
2026-07-15 11:45:21 +0900

    Update app.py

--- nougyousi-quiz/app.py
+++ nougyousi-quiz/app.py
@@ -13,7 +13,7 @@ questions = df.fillna("").to_dict(orient="records")
 
 def load_questions():
 
-    CSV_URL = "https://docs.google.com/spreadsheets/d/あなたのシートID/export?format=csv&gid=0"
+    CSV_URL = "https://docs.google.com/spreadsheets/d/1CeOOBmB4URYbQKe0oIhcRmsxp6oFZ3szC4r4k16N7PI/edit?gid=0#gid=0"
 
     df = pd.read_csv(CSV_URL, dtype=str)
 
